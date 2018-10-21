b6a0c3e9121132426bf1f1b2c54c3f6fdeb07719
Aaron Zemetres <azemetre@gmail.com>
2018-10-21 15:51:25 -0400

    feature: added battle results route and component

--- app/components/App.js
+++ app/components/App.js
@@ -7,6 +7,7 @@ var Switch = ReactRouter.Switch;
 var Nav = require('./nav');
 var Home = require('./Home');
 var Battle = require('./Battle');
+var Results = require('./Results');
 
 class App extends React.Component {
   render() {
@@ -16,7 +17,8 @@ class App extends React.Component {
           <Nav />
           <Switch>
             <Route exact path='/' component={Home} />
-            <Route path='/battle' component={Battle} />
+            <Route exact path='/battle' component={Battle} />
+            <Route path='/battle/results' component={Results} />
             <Route path='/popular' component={Popular} />
             <Route render={function () {
               return <p>Not Found homie</p>
