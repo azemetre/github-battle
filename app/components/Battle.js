9daa13d6da0e205d487da6286d8b3d65bfb6f809
Aaron Zemetres <azemetre@gmail.com>
2018-10-21 14:48:09 -0400

    feat: create routes, and popular repos component
    Navigation bar with appropriate routing as made, covers if
    user goes to nonexistent route.
    
    Popular component shows user the top repos for a few different languages

---
+++ app/components/Battle.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+class Battle extends React.Component {
+  render() {
+    return (
+      <div>
+        <span>FIGHT!</span>
+      </div>
+    )
+  }
+}
+
+module.exports = Battle;
